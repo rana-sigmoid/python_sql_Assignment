d42cb6234cad9d0424dee496f0de7c4560ffceff
rana-sigmoid <98724124+rana-sigmoid@users.noreply.github.com>
2022-03-10 11:15:10 +0530

    Update task2.py

--- problems/task2.py
+++ problems/task2.py
@@ -9,22 +9,21 @@ import logging
 import connectdb
 
 # task2
-# Write a python program to list the Total compensation  given till his/her last date or
-# till now of all the employees till date in a xlsx file.
+# Write a python program to list the Total compensation  given till his/her last date or till now of all the employees till date in a xlsx file.
 # columns required: Emp Name, Emp No, Dept Name, Total Compensation, Months Spent in Organization
 class Task2:
     def task2(self,cursor):
         # update the end date to current date to find the month spent of each employee who are in the orgenisation
-        update_end_date = "update jobhist set enddate=current_date where enddate=null"
+        update_end_date = "update jobhist set enddate=current_date where enddate is null"
         try:
             cursor.execute(update_end_date)  # this will execute the query
             logging.debug(f" updation executed on cursor - {cursor}")
         except:
             logging.error("failed to fetch ci=ursor from database")
 
-        query = "select emp.ename,emp.empno,dept.deptno, dept.dname,round((jh.enddate - jh.startdate)/30)*jh.sal as total_compensation," \
-                "date_part('month',age(jh.enddate, jh.startdate)) as " \
-                "emp_month_spent from emp join dept on emp.deptno = dept.deptno join jobhist as jh on emp.empno = jh.empno"
+        query = "select emp.ename,emp.empno, dept.dname,sum(round((jh.enddate - jh.startdate)/30)*jh.sal) as total_compensation," \
+                "date_part('month',age(jh.enddate, jh.startdate)) as emp_month_spent" \
+                "from emp join dept on emp.deptno = dept.deptno join jobhist as jh on emp.empno = jh.empno GROUP BY emp.empno, dept.dname;"
         try:
             cursor.execute(query)  # this will execute the query
             logging.debug(f" query executed on cursor - {cursor}")
@@ -33,7 +32,7 @@ class Task2:
             # extracting all data from cursor
         query_result = cursor.fetchall()
 
-        # print(query_result)
+        
 
         # inserting header in query result
         query_result.insert(0, [cursor.description[i].name for i in range(len(cursor.description))])
