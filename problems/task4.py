372d675c68a011c6f8e917cafd4df93a51a276ed
rana-sigmoid <98724124+rana-sigmoid@users.noreply.github.com>
2022-03-10 20:49:18 +0530

    Update task4.py

--- problems/task4.py
+++ problems/task4.py
@@ -9,12 +9,12 @@ import logging
 
 class Task4:
     def task4(self):
-        with pd.ExcelFile("../task2.xlsx") as xls:
+        with pd.ExcelFile("../Output/task2.xlsx") as xls:
             df = pd.read_excel(xls)
 
-        data = df.groupby('dname').agg({"total_compensation": sum}).reset_index().rename(
-            columns={"deptno": "Dept No", "dname": "Dept Name", "total_compensation": "Compensation"})
-        data.to_excel("/Users/ranadilendrasingh/PycharmProjects/pythonsqlproject/Output/task4.xlsx", header=True, index=False)
+            data = df.groupby('dname').agg({"total_compensation": sum}).reset_index().rename(
+                columns={"deptno": "Dept No", "dname": "Dept Name", "total_compensation": "Compensation"})
+            data.to_excel("/Users/ranadilendrasingh/PycharmProjects/pythonsqlproject/Output/task4.xlsx", header=True, index=False)
 
         # print(data)
 
