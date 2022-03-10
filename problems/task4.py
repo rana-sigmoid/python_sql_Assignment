f5405dcff47bbeedde1bc35cd4536f0748eeafd7
rana-sigmoid <98724124+rana-sigmoid@users.noreply.github.com>
2022-03-10 10:59:26 +0530

    Add files via upload

---
+++ problems/task4.py
@@ -0,0 +1,25 @@
+import psycopg2
+import os
+from sqlalchemy import create_engine
+import pandas as pd
+import config
+import pandas.io.sql as sql
+import pandasql as ps
+import logging
+
+class Task4:
+    def task4(self):
+        with pd.ExcelFile("../task2.xlsx") as xls:
+            df = pd.read_excel(xls)
+
+        data = df.groupby('dname').agg({"total_compensation": sum}).reset_index().rename(
+            columns={"deptno": "Dept No", "dname": "Dept Name", "total_compensation": "Compensation"})
+        data.to_excel("/Users/ranadilendrasingh/PycharmProjects/pythonsqlproject/task4.xlsx", header=True, index=False)
+
+        # print(data)
+
+if __name__ == "__main__":
+
+    obj_task=Task4()
+    obj_task.task4()
+
